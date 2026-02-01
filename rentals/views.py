3b5a9b9ecaab3843f7c977e550f95a1dfda5fc2b
bughunterpoojan <kamanipoojan@gmail.com>
2026-02-01 09:05:28 +0530

    fake paymentgateway fixed

--- rentals/views.py
+++ rentals/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponseForbidden, HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 from django.db import transaction
 from django.contrib import messages
 from datetime import datetime, timedelta
